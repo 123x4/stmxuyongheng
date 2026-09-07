292051a00262a4c5506e2026730a8461b336ead6
xyh <1840113472@qq.com>
2026-09-07 09:44:04 +0800

    加入串口模块

--- user/main.c
+++ user/main.c
@@ -4,13 +4,12 @@
 int main(void)
 {
 	Delay_Init();
-	
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE); // 开启GPIOC的时钟
 	
 	GPIO_InitTypeDef GPIO_InitStruct;
 	
 	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_13;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_OD;
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
 	
 	GPIO_Init(GPIOC, &GPIO_InitStruct);
@@ -21,6 +20,7 @@ int main(void)
 	
 	while(1)
 	{
-	
+		GPIO_WriteBit(GPIOC, GPIO_Pin_13, Bit_RESET); // 亮
+		
 	}
 }
