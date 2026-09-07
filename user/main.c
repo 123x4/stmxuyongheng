b204ff5fca864626d8b3b7b5bf6db91279f8bbaf
xyh <1840113472@qq.com>
2026-09-07 10:23:02 +0800

    布置学习环境

--- user/main.c
+++ user/main.c
@@ -1,26 +1,54 @@
 #include "stm32f10x.h"
-#include "delay.h"
+
+void My_I2C_Init(void);
+int My_I2C_SendBytes(I2C_TypeDef *I2Cx, uint8_t Addr, uint8_t *pData, uint16_t Size);
 
 int main(void)
 {
-	Delay_Init();
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE); // 开启GPIOC的时钟
+	My_I2C_Init();
 	
-	GPIO_InitTypeDef GPIO_InitStruct;
+	uint8_t commands[] = {0x00, 0x8d, 0x14, 0xaf, 0xa5};
 	
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_13;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_OD;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
-	
-	GPIO_Init(GPIOC, &GPIO_InitStruct);
-	
-	// GPIO_WriteBit(GPIOC, GPIO_Pin_13, Bit_SET);
-	
-	// GPIO_WriteBit(GPIOC, GPIO_Pin_13, Bit_RESET);
+	My_I2C_SendBytes(I2C1, 0x78, commands, 5);
 	
 	while(1)
 	{
-		GPIO_WriteBit(GPIOC, GPIO_Pin_13, Bit_RESET); // 亮
-		
 	}
+}
+
+void My_I2C_Init(void)
+{
+	// #1. IO引脚初始化
+	
+	// 对I2C1进行重映射
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+	
+	GPIO_PinRemapConfig(GPIO_Remap_I2C1, ENABLE);
+	
+	// 对PB8和PB9进行初始化
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	
+	GPIO_InitTypeDef GPIO_InitStruct;
+	
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF_OD;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
+	
+	GPIO_Init(GPIOB, &GPIO_InitStruct);
+	
+	// #2. 初始化I2C1模块
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
+	
+	RCC_APB1PeriphResetCmd(RCC_APB1Periph_I2C1, ENABLE); // 施加复位信号
+	RCC_APB1PeriphResetCmd(RCC_APB1Periph_I2C1, DISABLE); // 释放复位信号
+	
+	I2C_InitTypeDef I2C_InitStruct;
+	
+	I2C_InitStruct.I2C_ClockSpeed = 400000;
+	I2C_InitStruct.I2C_Mode = I2C_Mode_I2C;
+	I2C_InitStruct.I2C_DutyCycle = I2C_DutyCycle_2;
+	
+	I2C_Init(I2C1, &I2C_InitStruct);
+	
+	I2C_Cmd(I2C1, ENABLE); // 使能I2C1
 }
